9dbe6ba04ec5ae17891666cc6d14b6c2a2cb9229
Thiago-Cardoso <trovador182@gmail.com>
2018-05-14 22:28:17 -0300

    component_header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
- 
+import Header from './components/Header';
+
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello Onebitcode
-      </div>
+      <Header/>
     );
   }
 }
- 
+
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, NavItem, Row, Icon } from 'react-materialize';
+ 
+import styled from 'styled-components';
+import logo from '../../images/logo.png';
+ 
+const NavbarBlue = styled(Navbar)`
+  background-color: #75d3f4;`
+;
+ 
+const Logo = styled.img`
+  margin-top: 15px;`
+;
+ 
+const IconUser = styled(Icon)`
+  font-size: 40px !important;`
+;
+ 
+const Header = () => (
+  <Row>  
+    <NavbarBlue brand={<Logo src={logo} className="responsive-img col m6 center" />} right>
+      <NavItem href='/logout'>
+        Logout
+      </NavItem>
+      <NavItem href='/user/x/edit'>
+        <IconUser>account_circle</IconUser>
+      </NavItem>
+    </NavbarBlue>
+  </Row>
+);
+ 
+export default Header;
