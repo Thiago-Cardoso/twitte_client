f9ea7eb0c48fa15ae56ee38ed8bfb53e1b561a7f
Thiago-Cardoso <trovador182@gmail.com>
2018-07-01 22:51:50 -0300

    refactor trending topics

--- src/containers/TrendingTopicsContainer/reducer.js
+++ src/containers/TrendingTopicsContainer/reducer.js
@@ -1,11 +1,13 @@
+import { UPDATE_TRENDINGS } from './constants';
+
 // The initial state of the App
 const initialState = { hashtags: [] };
 
 export default function(state = initialState, action) {
   switch (action.type) {
-   case 'UPDATE_TRENDINGS':
-    return { hashtags: [['#OneBitCode', '0.1k'], ['#RubyOnRails', '10k']] }
+   case UPDATE_TRENDINGS:
+    return { ...state, hashtags: action.payload.hashtags }
    default:
-     return state;
+    return state;
   }
 }
