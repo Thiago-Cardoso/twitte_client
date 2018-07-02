4bc6c5801b9e5bb682df76f9ab751060e96ae348
Thiago-Cardoso <trovador182@gmail.com>
2018-07-01 22:40:06 -0300

    configure reduce

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
- import ReactDOM from 'react-dom';
- import './index.css';
- import App from './App';
- import registerServiceWorker from './registerServiceWorker';
- 
- import { createStore, combineReducers } from 'redux';
- import { Provider } from 'react-redux';
- import TrendingTopicsReducer from './containers/TrendingTopicsContainer/reducer.js';
- 
- const Reducers = combineReducers({
-  trendings: TrendingTopicsReducer
- });
- 
- const Store = createStore(Reducers);
- 
- 
- ReactDOM.render(
-   <Provider store={Store}>
-     <App/>
-   </Provider>
-   , document.getElementById('root'));
- registerServiceWorker();
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import Store from './configureStore';
+
+ReactDOM.render(
+ <Provider store={Store}>
+   <App/>
+ </Provider>
+ , document.getElementById('root')
+);
+
+registerServiceWorker();
