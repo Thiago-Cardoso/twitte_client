7b52a53ad6af5947f6826e608078d8fb5fe02da1
Thiago-Cardoso <trovador182@gmail.com>
2018-07-01 23:18:36 -0300

    add router

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import Reducers from './reducers';
 import thunk from 'redux-thunk';
-
-const Store = createStore(Reducers, applyMiddleware(thunk));
-
-export default Store;
+ 
+ // Routes imports
+ import createHistory from 'history/createBrowserHistory'
+ import { routerMiddleware } from 'react-router-redux'
+ 
+ export const history = createHistory()
+ const history_middleware = routerMiddleware(history)
+ // End routes imports
+ 
+ const Store = createStore(Reducers, applyMiddleware(history_middleware, thunk));
+ 
+ export default Store;
