26a7b7211c9ef15f1cce41222be16c891a502fc3
Thiago-Cardoso <trovador182@gmail.com>
2018-07-05 22:47:27 -0300

    component login

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -4,6 +4,8 @@ import { Row, Col, Container} from 'react-materialize';
 import logo from '../../images/logo.png';
 import background from '../../images/background.png';
 import styled from 'styled-components';
+import SignInContainer from '../../containers/Auth/SignInContainer';
+
 
 const BackgroundBlue = styled.div`
   background-color: #75d3f4;
@@ -27,20 +29,21 @@ const HomePage = () => (
         <Col xs={12} s={8} m={4} l={4}>
           <Logo src={logo} className="responsive-img"/>
         </Col>
-      </Row>
+       </Row>
       <Row>
-       <Col s={0} m={0} l={6}>
-        <Row>
-         <Col m={10} offset="m1" className="hide-on-med-and-down">
+     <Col s={0} m={0} l={6}>
+      <Row>
+        <Col m={10} offset="m1" className="hide-on-med-and-down">
           <BgImg src={background} className="responsive-img" />
-         </Col>
-        </Row>
-       </Col>
-      <Col s={12} m={8} offset="m2 l2" l={4}>
+        </Col>
+      </Row>
+     </Col>
+     <Col s={12} m={8} offset="m2 l2" l={4}>
+      <SignInContainer/>
      </Col>
     </Row>
    </Container>
   </BackgroundBlue>
-);
+ );
 
-export default HomePage;
+ export default HomePage;
