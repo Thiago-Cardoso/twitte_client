a08e9ba394d797cb503d97b70c9dea48926bf98c
Thiago-Cardoso <trovador182@gmail.com>
2018-07-10 23:57:11 -0300

    component_timeline and protect router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Route, Link, Switch } from 'react-router-dom';
+
 import HomePageContainer from './containers/HomePageContainer'
 import ProfilePageContainer from './containers/ProfilePageContainer'
+import TimelineContainer from './containers/TimelineContainer'
+import PrivateRoute from './containers/Auth/PrivateRoute'
 
 class App extends Component {
  render() {
@@ -10,6 +13,7 @@ class App extends Component {
      <Switch>
        <Route exact path="/" component={HomePageContainer} /> 
        <Route exact path="/user/:id" component={ProfilePageContainer} />
+       <PrivateRoute exact path="/timeline" component={TimelineContainer} /> 
      </Switch>
     </div>
    );
--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,10 @@
- import { combineReducers } from 'redux';
- import TrendingTopicsReducer from './containers/TrendingTopicsContainer/reducer.js';
- 
- const rootReducer = combineReducers({
-  trendings: TrendingTopicsReducer
- });
- 
- export default rootReducer;
+import { combineReducers } from 'redux';
+import TrendingTopicsReducer from './containers/TrendingTopicsContainer/reducer.js';
+import CurrentUserReducer from './containers/Auth/reducer.js';
+
+const rootReducer = combineReducers({
+ trendings: TrendingTopicsReducer,
+ current_user: CurrentUserReducer,
+});
+
+export default rootReducer;
