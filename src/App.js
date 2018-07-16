d26059b4f93116570740f44de486b6d370322f2a
Thiago-Cardoso <trovador182@gmail.com>
2018-07-16 19:29:39 -0300

    profile page userinfo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
- import React, { Component } from 'react';
- import { Route, Link, Switch } from 'react-router-dom';
- 
- import HomePageContainer from './containers/HomePageContainer'
- import ProfilePageContainer from './containers/ProfilePageContainer'
- import TimelineContainer from './containers/TimelineContainer'
- import ProfileEditContainer from './containers/ProfileEditContainer'
- import PrivateRoute from './containers/Auth/PrivateRoute'
- 
- class App extends Component {
+import React, { Component } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+
+import HomePageContainer from './containers/HomePageContainer'
+import ProfilePageContainer from './containers/ProfilePageContainer'
+import TimelineContainer from './containers/TimelineContainer'
+import ProfileEditContainer from './containers/ProfileEditContainer'
+import PrivateRoute from './containers/Auth/PrivateRoute'
+
+
+
+class App extends Component {
   render() {
-   return (
-    <div>
-     <Switch>
-      <Route exact path="/" component={HomePageContainer} /> 
-      <Route exact path="/user/:id" component={ProfilePageContainer} />
-      <PrivateRoute exact path="/timeline" component={TimelineContainer} />
-      <PrivateRoute exact path="/user/:id/edit" component={ProfileEditContainer} />
-     </Switch>
-    </div>
-   );
+    return (
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePageContainer} />            
+          <PrivateRoute exact path="/user/:id" component={ProfilePageContainer}  />
+          <PrivateRoute exact path="/timeline" component={TimelineContainer}  />
+          <PrivateRoute exact path="/user/:id/edit" component={ProfileEditContainer}  />
+        </Switch>
+      </div>
+    );
   }
- }
- 
- export default App;
+}
+
+export default App;
