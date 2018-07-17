40ce62c6975e1c507c33265c910c8895841bf45f
Thiago-Cardoso <trovador182@gmail.com>
2018-07-16 23:33:39 -0300

    manager_tweets

---
+++ src/containers/TweetListContainer/actions.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import API from '../../api.js';
+import { SET_TWEETS, DELETE_TWEET } from './constants';
+
+export function deleteTweet(id) {
+  const headers =  { 'Authorization': 'Bearer ' + localStorage.getItem('jwt') };
+  const request = API.delete(`/tweets/${id}`, {headers: headers});
+
+  return (dispatch) => {
+    request.then(
+      resp => dispatch({ type: DELETE_TWEET, payload: id }),
+      error => window.Materialize.toast('Problem in delete Tweet', 4000, 'red')
+    )
+  };
+}
